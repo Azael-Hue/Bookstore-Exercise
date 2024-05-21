de028e0d9dec6b5f53ccd6ba30288040735352fb
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-05-20 18:04:15 -0700

    Removed unneeded test code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 class Book {
 }
-let myBook = new Book();
-myBook.isbn = "123";
-myBook.price = 9.99;
-myBook.title = "Programming for beginners";
-myBook.releaseDate = new Date(2024, 5, 23);
-console.log(myBook);
 window.onload = function () {
     let addBookButton = document.querySelector("#add-book");
     addBookButton.onclick = processBook;
--- ts/main.ts
+++ ts/main.ts
@@ -24,16 +24,6 @@ class Book {
     releaseDate: Date;
 }
 
-// Book object test code
-let myBook = new Book();
-myBook.isbn = "123";
-myBook.price = 9.99;
-myBook.title = "Programming for beginners";
-myBook.releaseDate = new Date(2024, 5, 23); // Months start at index 0
-
-console.log(myBook);
-
-
 window.onload = function() {
     // Set up a button click for add book form
     let addBookButton = document.querySelector("#add-book") as HTMLButtonElement;
