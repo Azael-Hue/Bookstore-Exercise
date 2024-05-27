3d88c183c9a0b4fa4e1a83c91ab89ec2535bbb45
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-05-27 15:36:57 -0700

    Refactor addBook, single book is now added to localStorage on add

--- ts/main.ts
+++ ts/main.ts
@@ -33,7 +33,8 @@ window.onload = function() {
 function processBook() {
     let userBook = getBook();
     if (userBook != null) {
-        addBook(userBook);
+        addBookToWebpage(userBook);
+        addBookToStorage(userBook);
     }
 }
 
@@ -121,11 +122,11 @@ function isValidIsbn(data:string) {
 }
 
 /**
- * Adds a Book object to the web page and to web storage. Assumes
+ * Adds a Book object to the web page. Assumes
  * all data is valid
  * @param b The Book containing valid data to be added
  */
-function addBook(b: Book) {
+function addBookToWebpage(b: Book) {
     console.log(b);
 
     // Add the book to the web page
@@ -150,6 +151,33 @@ function addBook(b: Book) {
     // bookDisplayDiv.appendChild(bookDiv); // Add the newly created book
 
     document.querySelector("#book-display").appendChild(bookDiv);
+}
+
+/**
+ * Adds a single book object to existing storage.
+ * if no books are currently stored a new list will be created and stored
+ * @param b The book will be added to
+ */
+function addBookToStorage(b:Book):void {
+    const BookStorageKey = "Books";
+    // Read existing books out of storage
+    let bookData = localStorage.getItem(BookStorageKey);
+
+    // if bookData is null, the "Books" key did not exist
+    if (bookData == null) {
+        // Create a new list and add our current book
+        let books:Book[] = [];
+        books.push(b);
+
+        // Add a localStorage
+        bookData = JSON.stringify(books);
+        localStorage.setItem(BookStorageKey, bookData);
+    }
+    else{
+        // Parse string into a list of books and add new book to the list
+        // store the newly modified list back in storage
+    }
+
 }
 
 /**
